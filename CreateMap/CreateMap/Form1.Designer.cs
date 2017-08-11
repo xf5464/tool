eb20aff8de39909d92105dcd5da1e2038e00bed9
xf5464 <xf5464@gmail.com>
2017-08-11 18:43:23 +0800

    合成地图

---
+++ CreateMap/CreateMap/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CreateMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceBtn = new System.Windows.Forms.Button();
+            this.targetBtn = new System.Windows.Forms.Button();
+            this.sourceText = new System.Windows.Forms.Label();
+            this.outputPathText = new System.Windows.Forms.Label();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.mutilButton = new System.Windows.Forms.Button();
+            this.parentFolderOutputText = new System.Windows.Forms.Label();
+            this.mapParentFolderText = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.processText = new System.Windows.Forms.Label();
+            this.tileText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sourceBtn
+            // 
+            this.sourceBtn.Location = new System.Drawing.Point(12, 80);
+            this.sourceBtn.Name = "sourceBtn";
+            this.sourceBtn.Size = new System.Drawing.Size(75, 23);
+            this.sourceBtn.TabIndex = 0;
+            this.sourceBtn.Text = "单个地图";
+            this.sourceBtn.UseVisualStyleBackColor = true;
+            this.sourceBtn.Click += new System.EventHandler(this.sourceBtnClick);
+            // 
+            // targetBtn
+            // 
+            this.targetBtn.Location = new System.Drawing.Point(12, 109);
+            this.targetBtn.Name = "targetBtn";
+            this.targetBtn.Size = new System.Drawing.Size(75, 23);
+            this.targetBtn.TabIndex = 1;
+            this.targetBtn.Text = "输出目录";
+            this.targetBtn.UseVisualStyleBackColor = true;
+            this.targetBtn.Click += new System.EventHandler(this.outputBtnClick);
+            // 
+            // sourceText
+            // 
+            this.sourceText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sourceText.Location = new System.Drawing.Point(103, 85);
+            this.sourceText.Name = "sourceText";
+            this.sourceText.Size = new System.Drawing.Size(551, 18);
+            this.sourceText.TabIndex = 2;
+            // 
+            // outputPathText
+            // 
+            this.outputPathText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputPathText.Location = new System.Drawing.Point(103, 114);
+            this.outputPathText.Name = "outputPathText";
+            this.outputPathText.Size = new System.Drawing.Size(551, 18);
+            this.outputPathText.TabIndex = 3;
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(275, 153);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(97, 23);
+            this.generateBtn.TabIndex = 4;
+            this.generateBtn.Text = "单个地图开始";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.generateBtn_MouseClick);
+            // 
+            // mutilButton
+            // 
+            this.mutilButton.Location = new System.Drawing.Point(275, 271);
+            this.mutilButton.Name = "mutilButton";
+            this.mutilButton.Size = new System.Drawing.Size(97, 23);
+            this.mutilButton.TabIndex = 9;
+            this.mutilButton.Text = "多个地图开始";
+            this.mutilButton.UseVisualStyleBackColor = true;
+            this.mutilButton.Click += new System.EventHandler(this.mutilStartClick);
+            // 
+            // parentFolderOutputText
+            // 
+            this.parentFolderOutputText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.parentFolderOutputText.Location = new System.Drawing.Point(103, 234);
+            this.parentFolderOutputText.Name = "parentFolderOutputText";
+            this.parentFolderOutputText.Size = new System.Drawing.Size(551, 18);
+            this.parentFolderOutputText.TabIndex = 8;
+            // 
+            // mapParentFolderText
+            // 
+            this.mapParentFolderText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mapParentFolderText.Location = new System.Drawing.Point(103, 205);
+            this.mapParentFolderText.Name = "mapParentFolderText";
+            this.mapParentFolderText.Size = new System.Drawing.Size(551, 18);
+            this.mapParentFolderText.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "输出目录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.mutilOutputSelectClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "地图总目录";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.parentFolderClick);
+            // 
+            // processText
+            // 
+            this.processText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.processText.Location = new System.Drawing.Point(375, 271);
+            this.processText.Name = "processText";
+            this.processText.Size = new System.Drawing.Size(192, 23);
+            this.processText.TabIndex = 10;
+            // 
+            // tileText
+            // 
+            this.tileText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tileText.Location = new System.Drawing.Point(216, 26);
+            this.tileText.Name = "tileText";
+            this.tileText.Size = new System.Drawing.Size(192, 23);
+            this.tileText.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 386);
+            this.Controls.Add(this.tileText);
+            this.Controls.Add(this.processText);
+            this.Controls.Add(this.mutilButton);
+            this.Controls.Add(this.parentFolderOutputText);
+            this.Controls.Add(this.mapParentFolderText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.outputPathText);
+            this.Controls.Add(this.sourceText);
+            this.Controls.Add(this.targetBtn);
+            this.Controls.Add(this.sourceBtn);
+            this.Name = "Form1";
+            this.Text = "地图合成";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sourceBtn;
+        private System.Windows.Forms.Button targetBtn;
+        private System.Windows.Forms.Label sourceText;
+        private System.Windows.Forms.Label outputPathText;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.Button mutilButton;
+        private System.Windows.Forms.Label parentFolderOutputText;
+        private System.Windows.Forms.Label mapParentFolderText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label processText;
+        private System.Windows.Forms.Label tileText;
+    }
+}
+
---
+++ CreateMap/CreateMap/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace CreateMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            splits = new char[1];
+
+            splits[0] = '_';
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private Regex rgx = new Regex(@"\d+_\d+");
+
+        private char[] splits;
+
+        private void sourceBtnClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+
+                this.sourceText.Text = foldPath;
+            }
+        }
+
+        private int mapTileTotal = 0;
+
+        private int mapTileNowCount = 0;
+
+        private void drawToImage(Bitmap bitmap, List<FileInfo> files, String outputPath, String foldName)
+        {
+            Graphics formGraphics = Graphics.FromImage(bitmap);
+
+            int lastHIndex = 0;
+
+            int lastVIndex = 0;
+
+            int lastImageWidth = 0;
+
+            int lastImageHeight = 0;
+
+            int nowX = 0;
+
+            int nowY = 0;
+
+            mapTileNowCount = 0;
+
+            mapTileTotal = files.Count;
+
+            foreach (FileInfo NextFile in files)
+            {
+                Bitmap image1 = (Bitmap)Image.FromFile(NextFile.FullName, true);
+
+                String[] data = getHVIndex(NextFile);
+
+                if (data != null && data.Length >= 2)
+                {
+
+                    int hIndex = Convert.ToInt32(data[0]);
+
+                    int vIndex = Convert.ToInt32(data[1]);
+
+                    if (hIndex > lastHIndex)
+                    {
+                        nowX = 0;
+
+                        nowY += lastImageHeight;
+                    }
+
+
+                    TextureBrush texture = new TextureBrush(image1);
+                    texture.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+
+                    formGraphics.FillRectangle(texture,
+                        new RectangleF(nowX, nowY, image1.Width, image1.Height));
+
+                    lastHIndex = hIndex;
+
+                    lastVIndex = vIndex;
+
+                    lastImageWidth = image1.Width;
+
+                    lastImageHeight = image1.Height;
+
+                    nowX += image1.Width;
+                }
+
+                mapTileNowCount++;
+
+                this.tileText.Text = mapTileNowCount + "/" + mapTileTotal;
+
+                this.tileText.Invalidate();
+                this.tileText.Update();
+                //tileText.refresh();
+                Application.DoEvents();
+
+            }
+
+            String outputPathString = outputPath + "\\" + foldName + ".jpg";
+            bitmap.Save(outputPathString);
+
+            bitmap.Dispose();
+        }
+
+        private String getFoldeName(String folderName)
+        {
+
+            int index1 = folderName.LastIndexOf("\\");
+
+            String name = folderName.Substring(index1 + 1, folderName.Length - index1 - 1);
+
+            return name;
+        }
+
+        private int compareFileInfo(FileInfo a, FileInfo b)
+        {
+
+            String[] aHV = getHVIndex(a);
+
+            String[] bHV = getHVIndex(b);
+
+            if (aHV == null || aHV.Length < 2)
+            {
+                return -1;
+            }
+
+            if (bHV == null || bHV.Length < 2)
+            {
+                return 1;
+            }
+
+            int hIndex1 = Convert.ToInt32(aHV[0]);
+
+            int vIndex1 = Convert.ToInt32(aHV[1]);
+
+            int hIndex2 = Convert.ToInt32(bHV[0]);
+
+            int vIndex2 = Convert.ToInt32(bHV[1]);
+
+            if (hIndex1 < hIndex2)
+            {
+                return -1;
+            }
+            else if (hIndex1 > hIndex2)
+            {
+                return 1;
+            }
+            else
+            {
+                if (vIndex1 < vIndex2)
+                {
+                    return -1;
+                }
+                else if (vIndex1 > vIndex2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        }
+
+        private String[] getHVIndex(FileInfo file)
+        {
+            MatchCollection matches = rgx.Matches(file.FullName);
+
+            if (matches.Count > 0)
+            {
+                Match match = matches[0];
+
+                String[] data = match.Value.Split(splits);
+
+                return data;
+            }
+
+            return null;
+        }
+
+        private void outputBtnClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择输出文件夹路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+
+                this.outputPathText.Text = foldPath;
+            }
+        }
+
+        private void generateBtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            //MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            if (this.sourceText.Text == "")
+            {
+                MessageBox.Show("请选择地图目录");
+                return;
+            }
+
+            if (this.outputPathText.Text == "")
+            {
+                MessageBox.Show("请选择输出目录");
+                return;
+            }
+
+            generateOneMapFolder(this.sourceText.Text, this.outputPathText.Text);
+
+            MessageBox.Show("完成");
+        }
+
+        private void generateOneMapFolder(String path, String outputPath)
+        {
+            DirectoryInfo TheFolder = new DirectoryInfo(path);
+
+
+            //遍历文件
+            List<FileInfo> files = new List<FileInfo>();
+
+            FileInfo[] ret = TheFolder.GetFiles();
+
+            foreach (FileInfo temp in ret)
+            {
+                if (temp.FullName.IndexOf("_") == -1 || temp.FullName.ToLower().IndexOf(".jpg") == -1)
+                {
+                    continue;
+                }
+
+                files.Add(temp);
+            }
+
+            files.Sort(compareFileInfo);
+            //Array.Sort(files, compareFileInfo);
+
+            int totalWidth = 0;
+
+            int totalHeight = 0;
+
+            int lastHIndex = 0;
+
+            int lastVIndex = 0;
+
+            int lastImageWidth = 0;
+
+            int lastImageHeight = 0;
+
+            int nowX = 0;
+
+            int nowY = 0;
+
+            foreach (FileInfo NextFile in files)
+            {
+                Bitmap image1 = (Bitmap)Image.FromFile(NextFile.FullName, true);
+
+                String[] data = getHVIndex(NextFile);
+
+                if (data != null && data.Length >= 2)
+                {
+
+                    int hIndex = Convert.ToInt32(data[0]);
+
+                    int vIndex = Convert.ToInt32(data[1]);
+
+                    if (hIndex > lastHIndex)
+                    {
+                        if (totalWidth == 0)
+                        {
+                            totalWidth = nowX;
+                        }
+
+                        nowX = 0;
+
+                        nowY += lastImageHeight;
+                    }
+
+                    lastHIndex = hIndex;
+
+                    lastVIndex = vIndex;
+
+                    lastImageWidth = image1.Width;
+
+                    lastImageHeight = image1.Height;
+
+                    nowX += image1.Width;
+                }
+
+            }
+
+            totalHeight = nowY + lastImageHeight;
+
+            Bitmap bitmap = new Bitmap(Convert.ToInt32(totalWidth), Convert.ToInt32(totalHeight), System.Drawing.Imaging.PixelFormat.Format16bppRgb565);
+
+            String folderName = getFoldeName(path);
+
+            drawToImage(bitmap, files, outputPath, folderName);
+        }
+
+        private void mutilStartClick(object sender, EventArgs e)
+        {
+            if (this.mapParentFolderText.Text == "")
+            {
+                MessageBox.Show("请选择地图父目录");
+                return;
+            }
+
+            if (this.parentFolderOutputText.Text == "")
+            {
+                MessageBox.Show("请选择输出目录");
+                return;
+            }
+
+            DirectoryInfo TheFolder = new DirectoryInfo(this.mapParentFolderText.Text);
+
+            //遍历文件
+
+            DirectoryInfo[] directorys = TheFolder.GetDirectories();
+
+            totalMapCount = directorys.Length;
+
+            finishedMapCount = 0;
+
+            foreach (DirectoryInfo oneDirectory in directorys)
+            {
+                generateOneMapFolder(oneDirectory.FullName, this.parentFolderOutputText.Text);
+
+                finishedMapCount++;
+
+                this.processText.Text = finishedMapCount + "/" + totalMapCount;
+
+                this.processText.Invalidate();
+                this.processText.Update();
+                //tileText.refresh();
+                Application.DoEvents();
+            }
+
+            MessageBox.Show("完成");
+        }
+
+        private int totalMapCount = 0;
+
+        private int finishedMapCount = 0;
+
+        private void parentFolderClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择地图父目录";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+
+                this.mapParentFolderText.Text = foldPath;
+            }
+        }
+
+        private void mutilOutputSelectClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择输出文件夹路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+
+                this.parentFolderOutputText.Text = foldPath;
+            }
+        }
+
+    }
+}
