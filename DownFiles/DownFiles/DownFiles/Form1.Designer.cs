5916ac47e5e83bd7b83abbb40e5e32a91ad9ce41
xf5464 <xf5464@gmail.com>
2018-01-05 17:59:01 +0800

    类似charles的下载文件，按目录分开,url写在一个文件里

---
+++ DownFiles/DownFiles/DownFiles/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DownFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.sitesTxt = new System.Windows.Forms.TextBox();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.processTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "地址文本";
+            // 
+            // sitesTxt
+            // 
+            this.sitesTxt.Location = new System.Drawing.Point(117, 77);
+            this.sitesTxt.Name = "sitesTxt";
+            this.sitesTxt.Size = new System.Drawing.Size(409, 21);
+            this.sitesTxt.TabIndex = 3;
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(117, 113);
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.Size = new System.Drawing.Size(409, 21);
+            this.outputTxt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "输出目录";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.onStartClick);
+            // 
+            // processTxt
+            // 
+            this.processTxt.Location = new System.Drawing.Point(215, 146);
+            this.processTxt.Name = "processTxt";
+            this.processTxt.Size = new System.Drawing.Size(100, 12);
+            this.processTxt.TabIndex = 8;
+            this.processTxt.Text = "进度";
+            this.processTxt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 262);
+            this.Controls.Add(this.processTxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sitesTxt);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sitesTxt;
+        private System.Windows.Forms.TextBox outputTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label processTxt;
+    }
+}
+
---
+++ DownFiles/DownFiles/DownFiles/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace DownFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            regex = new Regex(@"(?<=(.com|.cn)/)(.*)(?=(/[^.*]).)");
+
+            nameRegex = new Regex(@"(?<=(.com|.cn)/)(.*)");
+
+        }
+
+        private Regex nameRegex;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private String localFolder;
+
+        private void onStartClick(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                return;
+            }
+
+            isRunning = true;
+
+            String webFile = this.sitesTxt.Text;
+
+            localFolder = this.outputTxt.Text;
+
+            data = File.ReadAllLines(webFile, Encoding.UTF8);
+
+            Array.Sort(data);
+
+            total = data.Length;
+
+            finished = 0;
+
+            downloadFile();
+        }
+
+        private Boolean isRunning = false;
+
+        private WebClient wclient;
+
+        private void downloadFile()
+        {
+            if (finished >= total)
+            {
+                MessageBox.Show("完成");
+
+                isRunning = false;
+            }
+            else
+            {
+                if (wclient == null)
+                {
+                     wclient = new WebClient();
+                }
+
+                String url = data[finished];
+
+                try
+                {
+                    String local = getLocalPath(url);
+
+                    if (local == "")
+                    {
+                        update();
+
+                        return;
+                    }
+
+
+                    String folder = localFolder + "/" + local;
+
+                    if (!Directory.Exists(folder))
+                    {
+                        Directory.CreateDirectory(folder);
+                    }
+
+                    String localName = getName(url);
+
+                    String fullName = localFolder + "/" + localName;
+
+                    wclient.DownloadFile(url, fullName);
+                }
+                catch (WebException error)
+                {
+                    Console.Write(error);
+
+                    update();
+
+                    return;
+                }
+
+                update();
+            }
+        }
+
+        private void update()
+        {
+            finished++;
+
+            updatefinishCount();
+
+            downloadFile();
+        }
+
+        private void updatefinishCount()
+        {
+            this.processTxt.Text = this.finished + "/" + this.total;
+
+            this.processTxt.Invalidate();
+            this.processTxt.Update();
+            //tileText.refresh();
+            Application.DoEvents();
+        }
+
+        private Regex regex;
+
+        private String getLocalPath(String remote)
+        {
+            MatchCollection mc = regex.Matches(remote);
+
+            if (mc.Count > 0)
+            {
+                return mc[0].Value;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private String getName(String remote)
+        {
+            MatchCollection mc = nameRegex.Matches(remote);
+
+            if (mc.Count > 0)
+            {
+                return mc[0].Value;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private int total;
+
+        private int finished;
+
+        private String[] data;
+    }
+}
