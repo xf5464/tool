8dd64af74653aa69e87fd8dda0cc6967410ccabe
xf5464 <xf5464@gmail.com>
2018-10-27 12:39:14 +0800

    1

---
+++ bingbing/pages/contact/contact.js
@@ -0,0 +1,85 @@
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    qqGroup:'691394132',
+    douyu:'628569',
+    music163:'抹抹茶冰阿冰',
+    weibo:'抹茶味Icecream'
+  },
+
+  copyText:function(e, text){
+    wx.setClipboardData({
+      data: e.target.dataset.copyValue,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            console.log(res.data) // data
+          }
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var i = 1;
+    var j = 2;
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+
+})
